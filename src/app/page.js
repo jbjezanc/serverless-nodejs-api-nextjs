00ab09ece9bad9690987ffa7f54c839ebdf19cde
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-09-27 00:28:28 +0200

    V2

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello There</h1>
+      <h1>Hello There V2</h1>
       <button className="bg-red-500 p-2 rounded-md" onClick={handleClick}>
         Click Me
       </button>
